8ec3a178c18afc34cba410716228d21e8ffae588
root_hbx <huboxuan2004@gmail.com>
2025-06-02 00:56:53 +0800

    chore: (main) rm time-costing tests && update .gitignore

--- StarPerf.py
+++ StarPerf.py
@@ -46,25 +46,25 @@ def main():
     samples.duration_constellation.duration_constellation_cases.constellation_performance()
     print("END.")
 
-    """
-    This will take the longest time. On a 4-core Intel Xeon Processor (Icelake) processor, 
-    1 second of traffic will be generated every 15 seconds. Therefore, the traffic generation 
-    here is set to 10 seconds, although I generated 1000 seconds of traffic in the experiment.
-    """
-    print("Start benign traffic generation")
-    import samples.traffic.traffic_generation_cases
-    samples.traffic.traffic_generation_cases.traffic_generation()
-    print("END.")
+    # """
+    # This will take the longest time. On a 4-core Intel Xeon Processor (Icelake) processor, 
+    # 1 second of traffic will be generated every 15 seconds. Therefore, the traffic generation 
+    # here is set to 10 seconds, although I generated 1000 seconds of traffic in the experiment.
+    # """
+    # print("Start benign traffic generation")
+    # import samples.traffic.traffic_generation_cases
+    # samples.traffic.traffic_generation_cases.traffic_generation()
+    # print("END.")
 
-    """
-    Energy consumption attacks require longer traffic generation time, because the implementation
-    period of the attack is usually measured in months and years. In the example here, I only 
-    implemented a simulation of dT=500 for one orbital period, which requires 11s of traffic generation.
-    """
-    print("Start LEO network security simulation")
-    import samples.attack.attack_cases
-    samples.attack.attack_cases.attack_cases()
-    print("END.")
+    # """
+    # Energy consumption attacks require longer traffic generation time, because the implementation
+    # period of the attack is usually measured in months and years. In the example here, I only 
+    # implemented a simulation of dT=500 for one orbital period, which requires 11s of traffic generation.
+    # """
+    # print("Start LEO network security simulation")
+    # import samples.attack.attack_cases
+    # samples.attack.attack_cases.attack_cases()
+    # print("END.")
     
 
 
